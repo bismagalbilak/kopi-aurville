7702d58a89045e675205e21257db8755cd81869b
bismaglbilak <bismaaezt2@gmail.com>
2023-06-30 15:15:26 +0700

    website-kopi-aurville

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// toogle
+const navbarNav = document.querySelector(".navbar-nav");
+
+// Ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// ketika hamburger menu diklik diluar atau di icon menunya
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
